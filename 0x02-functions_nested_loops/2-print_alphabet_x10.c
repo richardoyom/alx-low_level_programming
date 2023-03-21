52c973c25ae44329614f294774d92e98460025df
richardoyom <richard.oyom@yahoo.co.uk>
2023-03-20 23:14:23 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+	}
+}
