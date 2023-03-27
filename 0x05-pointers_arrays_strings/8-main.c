ad4365658dfb709d421fef81164af07f98960f87
richardoyom <richard.oyom@yahoo.co.uk>
2023-03-27 00:37:17 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int array[1] = {
+		98
+	};
+
+	print_array(array, 0);
+	print_array(array, -1);
+	return (0);
+    return (0);
+    return (0);
+}
