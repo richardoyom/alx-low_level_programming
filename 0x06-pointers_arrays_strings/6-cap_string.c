74382b0502f7ef53428a7de3dea181e025aa3849
richardoyom <richard.oyom@yahoo.co.uk>
2023-03-29 02:50:43 -0700

    6. Expect the best.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words in string
+ *
+ * @s: string param
+ * Return: char*
+ */
+
+char *cap_string(char *s)
+{
+int i;
+i = 0;
+
+while (s[i])
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!'
+|| s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')'
+|| s[i - 1] == '{' || s[i - 1] == '}' || i == 0)
+{
+s[i] = s[i] - 'a' + 'A';
+}
+}
+
+i++;
+}
+
+return (s);
+}
