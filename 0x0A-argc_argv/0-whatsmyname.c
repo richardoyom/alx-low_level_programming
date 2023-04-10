242920cf6e37689bee2875f2b21620880b505902
richardoyom <richard.oyom@yahoo.co.uk>
2023-04-09 23:45:55 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argv: string parameter
+ * @argc: argument count (unused)
+ * Return: 0
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
